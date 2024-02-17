c472f04ea1098c75320439ae24a7e5620ad86f91
basavrajnimbale <nimbalebasavraj1@gmail.com>
2024-02-17 22:35:46 +0530

    4th commit Relations between Tables

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
   User.findById("65d063a4350134b65e2050dc")
     .then(user => {
-      req.user = user;
+      req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
     .catch(err => console.log(err));
