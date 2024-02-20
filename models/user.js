ec60f1071960cf49172622a877650ada77017853
basavrajnimbale <nimbalebasavraj1@gmail.com>
2024-02-20 13:48:15 +0530

    12th commit Order System

--- models/user.js
+++ models/user.js
@@ -52,6 +52,11 @@ userSchema.methods.removeFromCart = function (productId) {
     return this.save();
 }
 
+userSchema.methods.clearCart = function () {
+    this.cart = { item: [] };
+    return this.save();
+}
+
 module.exports = mongoose.model('User', userSchema);
 // const mongodb = require('mongodb');
 // const getDb = require('../util/database').getDb;
